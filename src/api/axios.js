96e9655b438a9b188f7d3c2229698a709ea789fd
hyeseon-park <lightning350@naver.com>
2023-03-10 16:21:05 +0900

    배너 생성하기 및 api 연결

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://api.",
+  baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "",
+    api_key: "9fd3952ae5ec40d29ecc3be379bce428",
     language: "ko-KR",
   },
 });

4e3e07ffeb3c81b6416973bcc9db7c7711e5a54c
hyeseon-park <lightning350@naver.com>
2023-03-24 17:16:27 +0900

    swiper 추가

--- src/hooks/useOnClickOutside.js
+++ src/hooks/useOnClickOutside.js
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 function useOnClickOutside(ref, handler) {
   useEffect(() => {
     const listener = (event) => {
-      console.log("ref", ref.current);
       if (!ref.current || ref.current.contains(event.target)) {
         return;
       }
